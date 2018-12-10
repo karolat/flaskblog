327de347316263f54c11d5656c92f7ac6a45f662
karolat <karolat@gmail.com>
2018-12-10 17:50:21 -0600

    midway 7 with tabbing issues--copied code snippets from sample files

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -20,7 +20,7 @@ posts = [
         {
         'author': 'karolat',
         'title': 'Third Post',
-        'content': 'Feel a lot more sure.  And happy and hopeful!  And hope Shiro beats his fever soon <3',
+        'content': 'yeah!',
         'date_posted': '15 Nov, 2018'
     }
 
@@ -70,8 +70,20 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account')
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
